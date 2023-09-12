156d477b855bb40b7fd75c077d5af9d5fc3de434
JohnBryan2822 <johnbryan2822@gmail.com>
2023-09-12 09:59:15 +0200

    Spring starter

---
+++ S25L01-WebAppWithSpringBoot/src/test/java/org/javacoders/S25L01WebAppWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javacoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S25L01WebAppWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
