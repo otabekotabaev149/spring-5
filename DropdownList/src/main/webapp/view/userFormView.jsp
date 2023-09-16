ae265232dd6e0197962b629fd7370a9b1848768f
JohnBryan2822 <johnbryan2822@gmail.com>
2023-09-16 16:46:29 +0200

    Dropdown list and Radio buttons with Spring MVC

---
+++ DropdownList/src/main/webapp/view/userFormView.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+    <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>User Form</title>
+</head>
+<body>
+<form:form action="displayUserInfo" modelAttribute="user">
+	Name: <form:input path="name"/>
+	<p/>
+	Gender: <form:radiobuttons path="gender" items="${genderMap}"/>
+	<p/>
+	Country: <form:select path="country" items="${countryMap}"/>
+	<p/>
+	<input type="submit" value="Submit">
+	
+</form:form>
+</body>
+</html>
---
+++ InputAndRadio/src/main/java/org/javacoders/MainController.java
@@ -0,0 +1,27 @@
+package org.javacoders;
+
+import org.javacoders.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("userFormView");
+		User user = new User();
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+	
+	@PostMapping("/displayUserInfo")
+	public ModelAndView displayUserInfo(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView("displayUserInfo");
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+}
